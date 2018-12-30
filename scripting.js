d59091d3ce32c52e5a1307602ea41c97be56c03f
Aparimeya Taneja <at307@duke.edu>
2018-12-30 18:07:22 +0530

    freesixnine

--- scripting.js
+++ scripting.js
@@ -1,14 +1,15 @@
 //Main JavaScript File
 //Testimonials slider settings config (refer to owl carousel documentation online to make changes
+
 var sdmn = 0;
 
 function eradicate(target) {
     $('#sdmn' + target + '').remove();
 }
-document.getElementsByClassName("gallery-pictures")[0].scrollBy(300,0);
+
+document.getElementsByClassName("gallery-pictures")[0].scrollBy(300, 0);
 
 $(document).click(function (x) {
-
     sdmn++;
     $('body').append("<div class='ripple' style='left: " + x.pageX + "px;top: " + x.pageY + "px' id='sdmn" + sdmn + "'></div>");
     setTimeout(eradicate, 300, sdmn);
@@ -35,6 +36,11 @@ new ScrollMagic.Scene({triggerElement: "#parallax4"})
 
 var agilities = new Array($('.gallery-contain').length).fill(0);
 $(document).ready(function () {
+    $('.pre-load').toggleClass("loaded");
+   setTimeout(function () {
+       $('.pre-load').hide(0);
+   },2000);
+
     for (var mgk = 0; mgk < $('.gallery-pictures').length; mgk++) {
         var bumboy = $('.gallery-pictures').eq(mgk).children();
         if (mgk % 2 == 1) {
@@ -43,52 +49,62 @@ $(document).ready(function () {
         }
         else
             bumboy.css({"left": "0"});
-
         $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        autoplay: true,
-        autoplayHoverPause: true,
-        autoplayTimeout: 15000,
-        responsive: {
-            0: {
-                items: 1
+            loop: true,
+            margin: 10,
+            nav: true,
+            autoplay: true,
+            autoplayHoverPause: true,
+            autoplayTimeout: 15000,
+            responsive: {
+                0: {
+                    items: 1
+                }
             }
-        }
-    });
+        });
 
 
 //Custom slider buttons, change inner HTML to change shape/SVG (Not in owl documentation
-    $('.owl-prev').html("<i class=\"material-icons\">arrow_back_ios</i>");
-    $('.owl-next').html("<i class=\"material-icons\">arrow_forward_ios</i>");
+        $('.owl-prev').html("<i class=\"material-icons\">arrow_back_ios</i>");
+        $('.owl-next').html("<i class=\"material-icons\">arrow_forward_ios</i>");
 
 //Testimonial slider height rectifier
-    if (document.location.pathname.match(/[^\/]+$/)[0] == "index.html") {
-        var xix = $('.main-content').position().top + $('.main-content').height() + 60;
-        if ($(window).height() > 1140)
-            xix += 100;
-        $('.owl-carousel').css("top", xix + "px");
-    }
+        if (document.location.pathname.match(/[^\/]+$/)[0] == "index.html") {
+            var xix = $('.main-content').position().top + $('.main-content').height() + 60;
+            if ($(window).height() > 1140)
+                xix += 100;
+            $('.owl-carousel').css("top", xix + "px");
+        }
 
 //Rating custom auto-star generator
-    var eqo = $('.t-rating');
-    for (var x = 0; x < eqo.length; x++) {
-        var surefour = eqo.eq(x).html();
-        eqo.eq(x).html("");
-        for (var y = 1; y <= surefour; y++)
-            eqo.eq(x).append("<i class=\"fas fa-star\"></i>");
-        if (surefour % 1 > 0)
-            eqo.eq(x).append("<i class=\"fas fa-star-half\"></i>");
-    }
+        var eqo = $('.t-rating');
+        for (var x = 0; x < eqo.length; x++) {
+            var surefour = eqo.eq(x).html();
+            eqo.eq(x).html("");
+            for (var y = 1; y <= surefour; y++)
+                eqo.eq(x).append("<i class=\"fas fa-star\"></i>");
+            if (surefour % 1 > 0)
+                eqo.eq(x).append("<i class=\"fas fa-star-half\"></i>");
+        }
 
 //Custom on-click ripple effect
 
+
     }
+    $('.gallery-pictures').on('click', 'img', function () {
+        var boonk = $(this).attr('src');
+        $('.image-focused').attr('src', "" + boonk + "");
+        $('.image-focus').slideToggle(300);
+        $('.image-focused').toggleClass("reveal");
+    });
+    $('.image-focus').click(function () {
+        $('.image-focus').slideToggle(300);
+        $('.image-focused').toggleClass("reveal");
+    });
     $('section span').click(function () {
         $(this).toggleClass("active");
         $(this).find('i').toggleClass("swerve");
-        var xqc =  $('.gallery-info').width() + 75;
+        var xqc = $('.gallery-info').width() + 75;
         var iddqd = $('section span').index(this);
         $('.parallaxParent h1').eq(iddqd).toggleClass("black");
         var soon = $('.gallery-pictures:eq(' + iddqd + ')').children();
@@ -97,10 +113,9 @@ $(document).ready(function () {
             ksi = "right";
         else
             ksi = "left";
-
         for (var v = 0; v < soon.length; v++) {
             if (agilities[iddqd] == 0) {
-                soon.eq(v).css( "" + ksi + "","" + xqc + "px");
+                soon.eq(v).css("" + ksi + "", "" + xqc + "px");
                 xqc += soon.eq(v).width() + 75;
             }
             else
@@ -126,7 +141,7 @@ function rectifier(agilities) {
                 ksi = "left";
 
             for (var sen = 0; sen < gen.length; sen++) {
-                gen.eq(sen).css("" + ksi + "","" + nin + "px");
+                gen.eq(sen).css("" + ksi + "", "" + nin + "px");
                 nin += gen.eq(sen).width() + 75;
             }
         }
